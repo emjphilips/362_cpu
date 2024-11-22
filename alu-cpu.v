19d8ae27f4f47e2cd287454a259ca4e1a1ffd5af
emjphilips <94873763+emjphilips@users.noreply.github.com>
2024-11-22 14:11:29 -0600

    Update alu-cpu.v

--- alu-cpu.v
+++ alu-cpu.v
@@ -1,22 +1,23 @@
-module alu(
-    input [31:0] a,
-    input [31:0] b,
+module alu_cpu(
+    input [31:0] a, 
+    input [31:0] b, 
     input [2:0] alu_control,
     output reg [31:0] result,
     output reg zero,
-    input clk
+    
 );
+    
     always @(posedge clk) begin
         case (alu_control)
             3'b000: result <= a & b; //AND
             3'b001: result <= a | b; //OR
             3'b010: result <= a + b; //ADD
-            3'b110: result <= a - b; //SUB
+            3'b011: result <= a - b; //SUB
             3'b111: result <= a * b; //MUL
             
-            default: result <= 0;
+            default: result <= 32'b0;
         endcase
-        zero <= (result == 0);
+      //  zero <= (result == 0);
     end
 endmodule
 
